a0917be2e8a68ecbe1b600a3231de2e50c518b75
pkbl-aoiss <kaoiss0098@gmail.com>
2021-12-23 00:08:34 +0900

    initial commit

---
+++ src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/totoro.java
@@ -0,0 +1,16 @@
+package net.aoissx.mc.plugin.net.aoissx.mc.plugin.totoro.Net.aoissx.mc.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class totoro extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
