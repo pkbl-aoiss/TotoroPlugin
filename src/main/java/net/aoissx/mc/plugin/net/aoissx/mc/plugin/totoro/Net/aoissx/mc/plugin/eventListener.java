67df45d2ccb0c3748f594f6412abd599d8abe0cf
pkbl-aoiss <kaoiss0098@gmail.com>
2021-12-23 00:32:49 +0900

    Add Event

---
+++ src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/eventListener.java
@@ -0,0 +1,14 @@
+package net.aoissx.mc.plugin.net.aoissx.mc.plugin.totoro.Net.aoissx.mc.plugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+public class eventListener implements Listener {
+
+    @EventHandler
+    public void onSneak(PlayerToggleSneakEvent event){
+
+    }
+
+}
--- src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/totoro.java
+++ src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/totoro.java
@@ -1,18 +1,32 @@
 package net.aoissx.mc.plugin.net.aoissx.mc.plugin.totoro.Net.aoissx.mc.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class totoro extends JavaPlugin {
+
+    private static Plugin plugin;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("Start Totoro Plugin");
+
+        // Event registration
+        plugin = this;
+        Bukkit.getServer().getPluginManager().registerEvents(new eventListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Stop Totoro Plugin...");
+    }
+
+    public static Plugin getPlugin() {
+        return plugin;
     }
 }
