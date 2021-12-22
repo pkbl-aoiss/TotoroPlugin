2ec28f771134fb697404615df3531a9b5c2701bb
pkbl-aoiss <kaoiss0098@gmail.com>
2021-12-23 00:17:51 +0900

    Add README and update

--- src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/totoro.java
+++ src/main/java/net/aoissx/mc/plugin/net/aoissx/mc/plugin/totoro/Net/aoissx/mc/plugin/totoro.java
@@ -7,10 +7,12 @@ public final class totoro extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("Start Totoro Plugin");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Stop Totoro Plugin...");
     }
 }
